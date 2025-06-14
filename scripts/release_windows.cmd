1d2575fd110f0065eb0f6fda1c1d978fd7f67be0
KnickQueue <155283151+KnickQueue@users.noreply.github.com>
2025-06-14 18:56:01 -0400

    Improve Windows ARM64 build docs and script

--- scripts/release_windows.cmd
+++ scripts/release_windows.cmd
@@ -1,34 +1,32 @@
 @echo off
 setlocal enabledelayedexpansion
 
-if "%1" == "" (
+if "%1"=="" (
   echo Please specify x86 ^(32-bit^), x64 ^(64-bit^) or arm64 architecture in cmdline
   goto :eof
 )
 
-set BOTH=0
-if not "%1" == "x86" if not "%1" == "x64" if not "%1" == "arm64" set BOTH=1
-
-if %BOTH% == 1  (
+set ARCH=%1
+if /I "%ARCH%"=="x86" (
+  set QT=C:\Qt\5.13.2\msvc2017\
+  set VSARCH=x86
+  set CMAKE_ARCH=Win32
+) else if /I "%ARCH%"=="arm64" (
+  set QT=C:\Qt\5.13.2\msvc2017_arm64\
+  set VSARCH=x64_arm64
+  set CMAKE_ARCH=arm64
+) else if /I "%ARCH%"=="x64" (
+  set QT=C:\Qt\5.13.2\msvc2017_64\
+  set VSARCH=amd64
+  set CMAKE_ARCH=x64
+) else (
   echo Only x86 ^(32-bit^), x64 ^(64-bit^) or arm64 architectures are supported!
   goto :eof
 )
 
-set ARCH=%1
-if "%ARCH%" == "arm64" (
-  call "c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64_arm64
-) else (
-  call "c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %ARCH%
-)
+call "c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %VSARCH%
 
-if "%ARCH%" == "x86" (
-set QT=C:\Qt\5.13.2\msvc2017\
-) else if "%ARCH%" == "arm64" (
-set QT=C:\Qt\5.13.2\msvc2017_arm64\
-) else (
-set QT=C:\Qt\5.13.2\msvc2017_64\
-)
-set PATH=%QT%\bin;%PATH%
+set PATH=%QT%bin;%PATH%
 
 set ROOT="%~dp0.."
 set BUILD="%~dp0..\build\build\release"
@@ -44,10 +42,10 @@ if "%ERRORLEVEL%" equ "0" (
   set VERSION_COMMIT=%VERSION%-!COMMIT!
 )
 
-if "%ARCH%" == "x86" (
+if "%ARCH%"=="x86" (
   set TARGET="%~dp0\..\release\rclone-browser-%VERSION_COMMIT%-windows-32-bit"
   set TARGET_EXE="%~dp0\..\release\rclone-browser-%VERSION_COMMIT%-windows-32-bit"
-) else if "%ARCH%" == "arm64" (
+) else if "%ARCH%"=="arm64" (
   set TARGET="%~dp0\..\release\rclone-browser-%VERSION_COMMIT%-windows-arm64"
   set TARGET_EXE="%~dp0\..\release\rclone-browser-%VERSION_COMMIT%-windows-arm64"
 ) else (
@@ -67,14 +65,7 @@ if exist build rd /s /q build
 mkdir build
 cd build
 
-if "%ARCH%" == "x86" (
-cmake -G %CMAKEGEN% -A Win32 -DCMAKE_CONFIGURATION_TYPES="Release" -DCMAKE_PREFIX_PATH=%QT% ..
-) else if "%ARCH%" == "arm64" (
-cmake -G %CMAKEGEN% -A arm64 -DCMAKE_CONFIGURATION_TYPES="Release" -DCMAKE_PREFIX_PATH=%QT% ..
-) else (
-cmake -G %CMAKEGEN% -A x64 -DCMAKE_CONFIGURATION_TYPES="Release" -DCMAKE_PREFIX_PATH=%QT% ..
-) 
-
+cmake -G %CMAKEGEN% -A %CMAKE_ARCH% -DCMAKE_CONFIGURATION_TYPES="Release" -DCMAKE_PREFIX_PATH=%QT% ..
 cmake --build . --config Release
 popd
 
@@ -93,7 +84,7 @@ copy "%VCToolsRedistDir%\%ARCH%\Microsoft.VC142.CRT\msvcp140.dll" "%TARGET%\"
 copy "%VCToolsRedistDir%\%ARCH%\Microsoft.VC142.CRT\vcruntime140*.dll" "%TARGET%\"
 
 rem for Windows 32 bits build include relevant openssl libraries
-if "%ARCH%" == "x86" (
+if "%ARCH%"=="x86" (
 copy "c:\Program Files (x86)\openssl-1.1.1d-win32\libssl-1_1.dll" "%TARGET%\"
 copy "c:\Program Files (x86)\openssl-1.1.1d-win32\libcrypto-1_1.dll" "%TARGET%\"
 )
@@ -114,9 +105,9 @@ rem Inno Setup installer by https://github.com/jrsoftware/issrc
 rem in case user wants to install both 32bits and 64bits versions we need two AppId
 rem 64bits ;AppId={{0AF9BF43-8D44-4AFF-AE60-6CECF1BF0D31}
 rem 32bits ;AppId={{5644ED3A-6028-47C0-9796-29548EF7CEA3}
-if "%ARCH%" == "x86" (
+if "%ARCH%"=="x86" (
 "c:\Program Files (x86)\Inno Setup 6"\iscc "/dMyAppVersion=%VERSION%" "/dMyAppId={{5644ED3A-6028-47C0-9796-29548EF7CEA3}" "/dMyAppDir=rclone-browser-%VERSION_COMMIT%-windows-32-bit" "/dMyAppArch=x86" /O"../release" /F"rclone-browser-%VERSION_COMMIT%-windows-32-bit" rclone-browser-win-installer.iss
-) else if "%ARCH%" == "arm64" (
+) else if "%ARCH%"=="arm64" (
 "c:\Program Files (x86)\Inno Setup 6"\iscc "/dMyAppVersion=%VERSION%" "/dMyAppId={{0AF9BF43-8D44-4AFF-AE60-6CECF1BF0D31}" "/dMyAppDir=rclone-browser-%VERSION_COMMIT%-windows-arm64" "/dMyAppArch=arm64" /O"../release" /F"rclone-browser-%VERSION_COMMIT%-windows-arm64" rclone-browser-win-installer.iss
 ) else (
 "c:\Program Files (x86)\Inno Setup 6"\iscc "/dMyAppVersion=%VERSION%" "/dMyAppId={{0AF9BF43-8D44-4AFF-AE60-6CECF1BF0D31}" "/dMyAppDir=rclone-browser-%VERSION_COMMIT%-windows-64-bit" "/dMyAppArch=x64" /O"../release" /F"rclone-browser-%VERSION_COMMIT%-windows-64-bit" rclone-browser-win-installer.iss
